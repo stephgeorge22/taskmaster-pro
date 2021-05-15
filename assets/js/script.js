ec6a048324abadadc51799cf891698cd33496a7d
stephgeorge <stephanie.george22@gmail.com>
2021-05-14 22:20:11 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -57,11 +56,6 @@ var auditTask = function(taskEl) {
   if (moment().isAfter(time)) {
     $(taskEl).addClass("list-group-item-danger");
   }
-
-  // apply new class is task is near/over due date
-  if (moment().isAfter(time)) {
-    $(taskEl).addClass("list-group-item-danger");
-  }
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
@@ -75,19 +69,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log(event);
+    $(event.target).removeClass("dropover-active");
   },
 
-  update: function(event) {
+  update: function() {
     //array to store the task data in 
     var tempArr = [];
 
@@ -114,9 +110,6 @@ $(".card .list-group").sortable({
     // update array on tasks object and save
     tasks[arrName] = tempArr;
     saveTasks();
-  }, 
-  stop: function(event) {
-    $(this).removeClass("dropover");
   }
 });
 
@@ -125,12 +118,14 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log(ui);
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -151,13 +146,14 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
 
   if (taskText && taskDate) {
     createTask(taskText, taskDate, "toDo");
+
 
     // close modal
     $("#task-form-modal").modal("hide");
@@ -188,8 +184,7 @@ $(".list-group").on("click", "p", function() {
 $(".list-group").on("blur", "textarea", function () {
   // get the textarea's current value/text
   var text = $(this)
-  .val()
-  .trim();
+  .val();
 
   //get the parent ul's  id attribute
   var status = $(this)
@@ -285,6 +280,13 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
+// audit task due dates every 30 minutes
+setInterval(function() { 
+  $(".card .list-group-item").each(function() {
+    auditTask($(this));
+  });
+}, 18000000);
 
 // load tasks for the first time
 loadTasks();
